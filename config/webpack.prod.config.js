9ba120a2caf2a9d356c4c934a280fa935e9b9573
Josh Loftis <jshlfts32@gmail.com>
2018-06-10 16:48:09 -0400

    Updating webpack prod config

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,59 +1,55 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const WebpackMd5Hash = require('webpack-md5-hash');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 /* eslint-enable import/no-extraneous-dependencies */
 
 const client = {
   entry: {
-    client: './src/client/src/index.js',
+    client: "./src/client/src/index.js"
   },
-  target: 'web',
+  target: "web",
   output: {
-    filename: '[name].[hash].js',
-    path: path.resolve(__dirname, '..', 'dist/public'),
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "..", "dist/public")
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: { loader: 'babel-loader' },
-      },
-    ],
+        use: { loader: "babel-loader" }
+      }
+    ]
   },
   plugins: [
-    new CleanWebpackPlugin('dist', {}),
     new HtmlWebPackPlugin({
-      template: './src/client/public/index.html',
-      filename: 'index.html',
-      favicon: './src/client/public/icon.ico',
-    }),
-    new WebpackMd5Hash(),
-  ],
+      template: "./src/client/public/index.html",
+      filename: "index.html",
+      favicon: "./src/client/public/icon.ico"
+    })
+  ]
 };
 
 const server = {
   entry: {
-    server: './src/server/index.js',
+    server: "./src/server/index.js"
   },
-  target: 'node',
+  target: "node",
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, '..', 'dist'),
+    filename: "[name].js",
+    path: path.resolve(__dirname, "..", "dist")
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: { loader: 'babel-loader' },
-      },
-    ],
+        use: { loader: "babel-loader" }
+      }
+    ]
   },
-  externals: [nodeExternals()],
+  externals: [nodeExternals()]
 };
 
 module.exports = [client, server];
