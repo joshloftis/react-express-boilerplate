92ef7c29b035d5fbf3e7df0b35776ee4c1c15f3c
Josh Loftis <jshlfts32@gmail.com>
2018-06-11 22:11:51 -0400

    Updating for basic test suite

---
+++ config/webpack/webpack.prod.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const client = {
+  entry: {
+    client: './src/client/src/index.tsx',
+  },
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        exclude: /node_modules/,
+        use: { loader: 'ts-loader' },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, '..', 'dist/public'),
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/client/public/index.html',
+      filename: 'index.html',
+      favicon: './src/client/public/icon.ico',
+    }),
+  ],
+};
+
+const server = {
+  entry: {
+    server: './src/server/index.ts',
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: { loader: 'ts-loader' },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, '..', 'dist'),
+  },
+  externals: [nodeExternals()],
+};
+
+module.exports = [client, server];
